fc7cc256ef479cbf54c7055d4226e94001441c62
Evgeny Khodykine <evgeny.khodykin@yandex.ru>
2022-05-30 20:52:59 +0500

    script_changes

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 let lastOperand = 0;
 let operation = null;
 
-const inputWindow = document.getElementById('inputWindow');
+const inputWindow = document.querySelector('#inputWindow');
 
 
-document.getElementById('btn_clr').addEventListener('click', function () {
+document.querySelector('#btn_clr').addEventListener('click',()=> {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
 })
 
-document.getElementById('btn_sum').addEventListener('click', function () {
+document.querySelector('#btn_sum').addEventListener('click',()=> {
     lastOperand = parseInt(inputWindow.value);
     operation = 'sum';
     inputWindow.value = '';
 })
 
-document.getElementById('btn_dif').addEventListener('click', function () {
+document.querySelector('#btn_dif').addEventListener('click',()=> {
     lastOperand = parseInt(inputWindow.value);
     operation = 'dif';
     inputWindow.value = '';
 })
 
-document.getElementById('btn_mult').addEventListener('click', function () {
+document.querySelector('#btn_mult').addEventListener('click',()=> {
     lastOperand = parseInt(inputWindow.value);
     operation = 'mult';
     inputWindow.value = '';
 })
 
-document.getElementById('btn_division').addEventListener('click', function () {
+document.querySelector('#btn_division').addEventListener('click',()=> {
     lastOperand = parseInt(inputWindow.value);
     operation = 'division';
     inputWindow.value = '';
 })
 
-document.getElementById('btn_sqr').addEventListener('click', function () {
+document.querySelector('#btn_sqr').addEventListener('click',()=> {
     lastOperand = parseInt(inputWindow.value);
     operation = 'sqr';
     inputWindow.value = '';
 })
 
-document.getElementById('btn_calc').addEventListener('click', function () {
+document.querySelector('#btn_calc').addEventListener('click',()=> {
     if (operation === 'sum'){
         const result = lastOperand + parseInt(inputWindow.value);
         operation = 0;
@@ -70,33 +70,33 @@ document.getElementById('btn_calc').addEventListener('click', function () {
 
 
 
-document.getElementById('btn_1').addEventListener('click', function () {
+document.querySelector('#btn_1').addEventListener('click',()=> {
     inputWindow.value += '1';
 })
-document.getElementById('btn_2').addEventListener('click', function () {
+document.querySelector('#btn_2').addEventListener('click',()=> {
     inputWindow.value += '2';
 })
-document.getElementById('btn_3').addEventListener('click', function () {
+document.querySelector('#btn_3').addEventListener('click',()=> {
     inputWindow.value += '3';
 })
-document.getElementById('btn_4').addEventListener('click', function () {
+document.querySelector('#btn_4').addEventListener('click',()=> {
     inputWindow.value += '4';
 })
-document.getElementById('btn_5').addEventListener('click', function () {
+document.querySelector('#btn_5').addEventListener('click',()=> {
     inputWindow.value += '5';
 })
-document.getElementById('btn_6').addEventListener('click', function () {
+document.querySelector('#btn_6').addEventListener('click',()=> {
     inputWindow.value += '6';
 })
-document.getElementById('btn_7').addEventListener('click', function () {
+document.querySelector('#btn_7').addEventListener('click',()=> {
     inputWindow.value += '7';
 })
-document.getElementById('btn_8').addEventListener('click', function () {
+document.querySelector('#btn_8').addEventListener('click',()=> {
     inputWindow.value += '8';
 })
-document.getElementById('btn_9').addEventListener('click', function () {
+document.querySelector('#btn_9').addEventListener('click',()=> {
     inputWindow.value += '9';
 })
-document.getElementById('btn_0').addEventListener('click', function () {
+document.querySelector('#btn_0').addEventListener('click',()=> {
     inputWindow.value += '0';
 })
